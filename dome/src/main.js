2bd5850b4d7a33faf874a19b34a4ff10c7f2cf09
jialang <907474402@qq.com>
2021-11-11 22:33:30 +0800

    黑马头条

---
+++ dome/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import './registerServiceWorker'
+import router from './router'
+import store from './store'
+//加载vant核心组件库
+import Vant from 'vant';
+// 加载vant全局样式
+import 'vant/lib/index.css';
+// 注册使用vant组件库
+Vue.use(Vant);
+//加载全局样式
+import './styles/index.less'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
